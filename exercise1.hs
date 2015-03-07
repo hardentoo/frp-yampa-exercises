4ae4b2492f6f5b7c8fb9b918b92d2806d565bb42
leohaskell <leohaskell98@gmail.com>
2015-03-07 11:56:03 +0200

    removing redundant restrictions from type signatures

--- exercise1.hs
+++ exercise1.hs
@@ -8,14 +8,14 @@ class Arr a where
 
   first :: a b c -> a (b, d) (c, d)
 
-  second :: Arr a => a b c -> a (d, b) (d, c)
+  second :: a b c -> a (d, b) (d, c)
   second f = arr swap >>> first f >>> arr swap
     where swap = \(x, y) -> (y, x)
 
-  (***) :: Arr a => a b c -> a b' c' -> a (b, b') (c, c')
+  (***) :: a b c -> a b' c' -> a (b, b') (c, c')
   f *** g = first f >>> second g
 
-  (&&&) :: Arr a => a b c -> a b d -> a b (c, d)
+  (&&&) :: a b c -> a b d -> a b (c, d)
   f &&& g = arr (\b -> (b, b)) >>> (f *** g)
 
 
